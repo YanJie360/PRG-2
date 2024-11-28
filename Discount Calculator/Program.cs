a692c9e05943c9c7d06e4d68479292851709ef3d
Liauw Yan Jie <s10267962@connect.np.edu.sg>
2024-11-28 18:08:14 +0800

    Add files via upload

---
+++ Discount Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter amount    ($): ");
+double amount = Convert.ToSingle(Console.ReadLine());
+
+double discount_Given;
+if (amount <= 100)
+{
+    discount_Given = 0;
+}
+else if (amount <= 500)
+{
+    discount_Given = 0.05;
+}
+else if (amount <= 1000)
+{ 
+    discount_Given = 0.1;
+}
+else
+{
+    discount_Given = 0.2;
+}
+
+double discount_Amount = amount * discount_Given;
+
+Console.WriteLine($"Discount given  (%): {discount_Given * 100}");
+Console.WriteLine($"Discount amount ($): {discount_Amount:F2}");
